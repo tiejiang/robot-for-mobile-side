c1762a310d587dd6dd4a20fe0e701b1cb0755a37
yinyu-tj <315904145@qq.com>
2017-06-26 15:49:53 +0800

    copy the yun-zhi-sheng demo to application, and now the project include voice input function and offlineTTS function

---
+++ src/main/java/com/yinyutech/xiaolerobot/config/Config.java
@@ -0,0 +1,19 @@
+/**
+ * Config.java [V 1.0.0]
+ * classes : cn.yunzhisheng.prodemo.Config
+ * liujunjie Create  at 2015-1-9  ??4:30:32
+ */
+package com.yinyutech.xiaolerobot.config;
+
+/**
+ * cn.yunzhisheng.prodemo.Config
+ * 
+ * @author liujunjie <br/>
+ *         Create at 2015-1-9 ??4:30:32
+ * 
+ */
+public class Config {
+
+	public static final String appKey = "5eqvls3cp6sfphmowioblbyv7o3zlxjdgyhlbaaj";
+	public static final String secret = "b2c4b6d225b4beba861918d4c3777f52";
+}
--- src/main/java/com/yinyutech/xiaolerobot/ui/MainActivity.java
+++ src/main/java/com/yinyutech/xiaolerobot/ui/MainActivity.java
@@ -1,16 +1,39 @@
 package com.yinyutech.xiaolerobot.ui;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.yinyutech.xiaolerobot.R;
 
 public class MainActivity extends Activity {
 
+    private Button startLocalTTS, voiceInput;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startLocalTTS = (Button)findViewById(R.id.begin_tts);
+        voiceInput = (Button)findViewById(R.id.voice_input);
+        startLocalTTS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(MainActivity.this, TTSOfflineActivity.class);
+                startActivity(mIntent);
+
+            }
+        });
+        voiceInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(MainActivity.this, ASROnlineActivity.class);
+                startActivity(mIntent);
+            }
+        });
 
 
     }
