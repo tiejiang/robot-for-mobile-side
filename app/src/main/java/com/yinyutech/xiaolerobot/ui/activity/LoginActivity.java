637f3bb00240cda54524caa6f77faf83f435cb27
tiejiang <315904145@qq.com>
2017-07-13 15:08:27 +0800

    小乐APP的基本TAB框架完成，下一个版本开始短信注册的开发

---
+++ app/src/main/java/com/yinyutech/xiaolerobot/ui/activity/LoginActivity.java
@@ -0,0 +1,32 @@
+package com.yinyutech.xiaolerobot.ui.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by yinyu-tiejiang on 17-7-13.
+ */
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
