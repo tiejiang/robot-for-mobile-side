8652cfb617764c8d266b80cd2536ed639f6e7a22
tiejiang <315904145@qq.com>
2017-07-13 18:43:09 +0800

    短信验证部分功能做了添加，但是还没完成，当日代码提交

--- app/src/main/java/com/yinyutech/xiaolerobot/XiaoLeApplication.java
+++ app/src/main/java/com/yinyutech/xiaolerobot/XiaoLeApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 
+import com.mob.MobSDK;
 import com.yinyutech.xiaolerobot.bean.User;
 import com.yinyutech.xiaolerobot.utils.UserLocalData;
 
@@ -11,18 +12,12 @@ import com.yinyutech.xiaolerobot.utils.UserLocalData;
 public class XiaoLeApplication extends Application {
 
     private User user;
-
-
-
     private static XiaoLeApplication mInstance;
-
 
     public static XiaoLeApplication getInstance(){
 
         return  mInstance;
     }
-
-
 
     @Override
     public void onCreate() {
@@ -31,22 +26,19 @@ public class XiaoLeApplication extends Application {
         mInstance = this;
         initUser();
 //        Fresco.initialize(this);  //facebook
+        //sharedSDK 初始化信息
+        MobSDK.init(getInstance(), "1f39c57121ae6", "62f9a52a61b3eaf0ab04d48189913694");
     }
-
-
-
 
     private void initUser(){
 
         this.user = UserLocalData.getUser(this);
     }
 
-
     public User getUser(){
 
         return user;
     }
-
 
     public void putUser(User user,String token){
         this.user = user;
@@ -59,16 +51,12 @@ public class XiaoLeApplication extends Application {
         UserLocalData.clearUser(this);
         UserLocalData.clearToken(this);
 
-
     }
-
 
     public String getToken(){
 
         return  UserLocalData.getToken(this);
     }
-
-
 
     private  Intent intent;
     public void putIntent(Intent intent){
--- app/src/main/java/com/yinyutech/xiaolerobot/ui/activity/LoginActivity.java
+++ app/src/main/java/com/yinyutech/xiaolerobot/ui/activity/LoginActivity.java
@@ -1,18 +1,42 @@
 package com.yinyutech.xiaolerobot.ui.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.yinyutech.xiaolerobot.R;
+
+import java.util.HashMap;
+
+import cn.smssdk.EventHandler;
+import cn.smssdk.SMSSDK;
 
 /**
  * Created by yinyu-tiejiang on 17-7-13.
  */
 
 public class LoginActivity extends Activity {
+
+    private EditText et_number;
+    private String number;
+    private EditText et_security;
+    private ProgressDialog dialog;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+        et_number = (EditText) findViewById(R.id.et_phone);
+        et_security = (EditText) findViewById(R.id.et_security);
+        //注册短信回调监听
+        SMSSDK.registerEventHandler(ev);
     }
 
     @Override
@@ -28,5 +52,89 @@ public class LoginActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        //要在activity销毁时反注册，否侧会造成内存泄漏问题
+        SMSSDK.unregisterAllEventHandler();
+    }
+    /**
+     * 短信验证的回调监听
+     */
+    private EventHandler ev = new EventHandler() {
+        @Override
+        public void afterEvent(int event, int result, Object data) {
+            if (result == SMSSDK.RESULT_COMPLETE) { //回调完成
+                //提交验证码成功,如果验证成功会在data里返回数据。data数据类型为HashMap<number,code>
+                if (event == SMSSDK.EVENT_SUBMIT_VERIFICATION_CODE) {
+                    Log.e("TAG", "提交验证码成功" + data.toString());
+                    HashMap<String, Object> mData = (HashMap<String, Object>) data;
+                    String country = (String) mData.get("country");//返回的国家编号
+                    String phone = (String) mData.get("phone");//返回用户注册的手机号
+
+                    Log.e("TAG", country + "====" + phone);
+
+                    if (phone.equals(number)) {
+                        runOnUiThread(new Runnable() {//更改ui的操作要放在主线程，实际可以发送hander
+                            @Override
+                            public void run() {
+                                showDailog("恭喜你！通过验证");
+                                dialog.dismiss();
+                                //    Toast.makeText(MainActivity.this, "通过验证", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    } else {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                showDailog("验证失败");
+                                dialog.dismiss();
+                                //     Toast.makeText(MainActivity.this, "验证失败", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                } else if (event == SMSSDK.EVENT_GET_VERIFICATION_CODE) {//获取验证码成功
+                    Log.e("TAG", "获取验证码成功");
+                } else if (event == SMSSDK.EVENT_GET_SUPPORTED_COUNTRIES) {//返回支持发送验证码的国家列表
+
+                }
+            } else {
+                ((Throwable) data).printStackTrace();
+            }
+        }
+    };
+    //验证结果弹窗
+    private void showDailog(String text) {
+        new AlertDialog.Builder(this)
+                .setTitle(text)
+                .setPositiveButton("确定", null)
+                .show();
+    }
+    /**
+     * 获取验证码
+     * @param v
+     */
+    public void getSecurity(View v) {
+        number = et_number.getText().toString().trim();
+        //发送短信，传入国家号和电话---使用SMSSDK核心类之前一定要在MyApplication中初始化，否侧不能使用
+        if (TextUtils.isEmpty(number)) {
+            Toast.makeText(this, "号码不能为空！", Toast.LENGTH_SHORT).show();
+        } else {
+            SMSSDK.getVerificationCode("+86", number);
+            Toast.makeText(this, "发送成功:" + number, Toast.LENGTH_SHORT).show();
+        }
+    }
+    /**
+     * 向服务器提交验证码，在监听回调中判断是否通过验证
+     * @param v
+     */
+    public void testSecurity(View v) {
+        String security = et_security.getText().toString();
+        if (!TextUtils.isEmpty(security)) {
+            dialog = ProgressDialog.show(this, null, "正在验证...", false, true);
+            //提交短信验证码
+            SMSSDK.submitVerificationCode("+86", number, security);//国家号，手机号码，验证码
+            Toast.makeText(this, "提交了注册信息:" + number, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "验证码不能为空", Toast.LENGTH_SHORT).show();
+        }
     }
 }
